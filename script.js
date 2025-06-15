8d2dde524541a54a05abba2acb0103d34ec8e568
Jano844 <jani.saenger@gmail.com>
2025-06-15 04:19:31 +0200

    now Testing Roles

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const playerNames = [];
 let playersCount = 0;
 let underCoverNum = 0
 let mrWhiteNum = 0
+let civilianNum = 0
+
+let tempUnderCoverNum
+let tempMrWhiteNum
+let tempCivilianNum
 
 
 const liste = [
@@ -58,6 +63,7 @@ function createPlayerIcons(number) {
 }
 
 function enableNameInput() {
+  resetTempRoles()
   const container = document.getElementById('imagesContainer');
   const images = container.querySelectorAll('img');
 
@@ -72,7 +78,7 @@ function enableNameInput() {
 
 
       // Give Player His Word
-      alert("Hallo, das ist ein Popup!");
+      print_word_of_player(playersCount + 1)
 
 
 
@@ -99,7 +105,7 @@ function enableNameInput() {
       // Klick entfernen
       img.removeEventListener('click', handler);
       playersCount = playersCount + 1
-      console.log(playersCount, playerNumber)
+    //   console.log(playersCount, playerNumber)
       if (playersCount == playerNumber) {
           document.querySelector('.enter_names').style.display = 'none';
 
@@ -164,3 +170,113 @@ function createLogic() {
 
 // Event Listener setzen
 document.getElementById('numberForm').addEventListener('submit', handleNumberSubmit);
+
+
+function resetTempRoles() {
+
+    civilianNum = playerNumber - underCoverNum - mrWhiteNum
+
+    tempMrWhiteNum = mrWhiteNum
+    tempUnderCoverNum = underCoverNum
+    tempCivilianNum = civilianNum
+}
+
+function getRandomLine(list) {
+  const randomIndex = Math.floor(Math.random() * list.length);
+  return list[randomIndex];
+}
+
+
+function getRoleForPlayer(num) {
+  // Berechnung von civilianNum aktuell (aktuelle Civilians im Spiel)
+
+
+  console.log("player num", playerNumber)
+  console.log("Civ Num", tempCivilianNum)
+  console.log("under Num", tempUnderCoverNum)
+  console.log("mr num", tempMrWhiteNum)
+
+  // Falls num == 1: kein Mr White möglich
+  if (num === 1) {
+    if (tempCivilianNum > 0 && tempUnderCoverNum > 0) {
+      const randomNum = Math.floor(Math.random() * 2) + 1;
+      if (randomNum === 1)
+      {
+        tempCivilianNum -= 1
+        return "Civilian";
+      }
+
+      else
+      {
+        tempUnderCoverNum -= 1
+        return "Undercover";
+      }
+
+    }
+    else if (tempCivilianNum > 0)
+    {
+      tempCivilianNum -= 1
+      return "Civilian";
+    }
+    else
+    {
+      tempUnderCoverNum -= 1
+      return "Undercover";
+    }
+
+  }
+  else {
+    // Für alle anderen Spieler: MrWhite möglich
+
+    // Sammle alle verfügbaren Rollen
+    const roles = [];
+    if (tempCivilianNum > 0) roles.push("Civilian");
+    if (tempUnderCoverNum > 0) roles.push("Undercover");
+    if (tempMrWhiteNum > 0) roles.push("Mr White");
+
+    if (roles.length === 0) {
+      return "No role available";
+    }
+
+    // Zufällig eine Rolle aus den verfügbaren wählen
+    const randomIndex = Math.floor(Math.random() * roles.length);
+    const role = roles[randomIndex];
+
+    // Zähle Rolle runter
+    switch (role) {
+      case "Civilian":
+        tempCivilianNum -= 1
+        break;
+      case "Undercover":
+        tempUnderCoverNum -= 1
+        break;
+      case "Mr White":
+        tempMrWhiteNum -= 1 
+        break;
+    }
+
+    return role;
+  }
+
+  return "No role assigned";
+}
+
+
+function print_word_of_player(num) {
+  const role = getRoleForPlayer(num);
+  
+  const randomLine = liste[Math.floor(Math.random() * liste.length)];
+//   console.log(randomLine)
+  
+  let wordToShow;
+
+  if (role === "MrWhite") {
+    wordToShow = "Du bist Mr White";
+  } else if (role === "Civilian") {
+    wordToShow = randomLine[0];
+  } else if (role === "Undercover") {
+    wordToShow = randomLine[1];
+  }
+
+  alert(`${wordToShow}`);
+}
